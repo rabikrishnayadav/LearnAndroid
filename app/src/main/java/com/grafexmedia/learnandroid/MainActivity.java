e7aa8da8a019bb48f382528f69f93b56ad71457e
RabiKrYadav <rabikrishnayadav@gmail.com>
2022-08-17 20:42:41 +0530

    New Project Created for Learning Android Studio

---
+++ app/src/main/java/com/grafexmedia/learnandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.grafexmedia.learnandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
